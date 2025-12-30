7cdd6a2784cf3e45a3d21bca5ba2c7f5c0076052
Junhyungkim <wnsgud4553@gmail.com>
2025-12-30 18:07:34 +0900

    Update extract_orders.sql AND dags/extract_dag

--- dags/extract_dag.py
+++ dags/extract_dag.py
@@ -2,18 +2,18 @@ from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime
 
-default_args = {"start_date": datetime(2025, 12, 29)}
+default_args = {"start_date": datetime(2025, 12, 30)}
 
 with DAG(
-    'postgres_extractor',
-    description='PostgreSQL extract',
+    'postgres_etl',
+    description='Extract, transform, and load data using PostgreSQL.',
     default_args=default_args,
     schedule='0 0 * * *',
     catchup=False
 ) as dag:
 
     postgres_task = PostgresOperator(
-        task_id = 'execute_sql_query',
+        task_id = 'extract_from_postgres',
         postgres_conn_id='my_postgres_connection',
         sql="sql/extract_orders.sql"
     )
--- dags/sql/extract_orders.sql
+++ dags/sql/extract_orders.sql
@@ -1,7 +1,20 @@
+INSERT INTO staging.order (
+    order_id,
+    user_id,
+    order_amount,
+    order_time,
+    status
+)
 SELECT  
     order_id,
     user_id,
     order_amount,
     order_time,
     status
-FROM raw."order";
+FROM raw."order"
+ON CONFLICT (order_id)
+DO UPDATE SET
+    user_id      = EXCLUDED.user_id,
+    order_amount = EXCLUDED.order_amount,
+    order_time   = EXCLUDED.order_time,
+    status       = EXCLUDED.status;
